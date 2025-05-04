88f1ce68b7480c39d004f0eda4c82ccf5d78d8ea
UcokBaret <20102050093@student.uin-suka.ac.id>
2025-05-04 20:51:30 +0700

    Update noez_KVM.sh

--- noez_KVM.sh
+++ noez_KVM.sh
@@ -33,8 +33,8 @@ apt update
 #apt upgrade -y
 echo "✅ Update dan upgrade selesai."
 
-echo "📦 Install aplikasi penting..."
-apt-get install -y ccze curl tcpdump python3-pip htop nodejs npm net-tools docker.io xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp
+echo "📦 Install aplikasi penting...python3-pip npm "
+apt-get install -y ccze curl tcpdump htop nodejs net-tools docker.io xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp
 apt autoremove -y
 echo "✅ Semua aplikasi penting sudah diinstall."
 
