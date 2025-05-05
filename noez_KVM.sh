e8ef9ce8137e15246cddb1a5a9edd6e1ed426d76
UcokBaret <20102050093@student.uin-suka.ac.id>
2025-05-05 14:06:28 +0700

    Update noez_KVM.sh

--- noez_KVM.sh
+++ noez_KVM.sh
@@ -82,8 +82,8 @@ echo "✅ Docker Repocket berjalan."
 #echo "✅ Mysteriumnetwork Node berhasil diinstall."
 
 echo "🧹 Membersihkan APT cache Dan File DEB..."
-apt remove unattended-upgrades xfce4-power-manager -y
-apt autoremove -y
+apt remove xfce4-power-manager -y
+apt autoremove unattended-upgrades -y
 apt clean
 apt autoclean
 rm -rf *.deb *.sh
